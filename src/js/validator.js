e5d224da422608353c6ad13e261cebaadf9f312e
Vassilyev Denis <tarapiygin@gmail.com>
2020-07-23 20:32:06 +0300

    rename const

--- src/js/validator.js
+++ src/js/validator.js
@@ -1,8 +1,8 @@
 export default class Validator {
   static validateUsername(name) {
-    const noForbiddenCharacters = !(/[^(a-zA-Z\d-_)]/.test(name));
-    const noThreeNumbers = !(/\d{3,}/.test(name));
-    const startsAndEndsWithLetters = !(/^[-_\d]|[-_\d]$/.test(name));
-    return noForbiddenCharacters && noThreeNumbers && startsAndEndsWithLetters;
+    const forbiddenCharacters = /[^(a-zA-Z\d-_)]/.test(name);
+    const threeNumbers = /\d{3,}/.test(name);
+    const doesNotStartAndEndWithLetters = /^[-_\d]|[-_\d]$/.test(name);
+    return !forbiddenCharacters && !threeNumbers && !doesNotStartAndEndWithLetters;
   }
 }
